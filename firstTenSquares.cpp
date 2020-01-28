50234f33ff0d3123ba7ad45cb7ba9d929c9d60f4
Dylan-Sullivan <50437257+Dylan-Sullivan@users.noreply.github.com>
2020-01-28 03:26:26 -0800

    added new cpp file

---
+++ firstTenSquares.cpp
@@ -0,0 +1,13 @@
+// Program that prints the first ten squares
+
+#include <iostream>
+
+int main(){
+  int i = 1;
+
+  while(i <= 10){
+    std::cout << i + " squared = " + i^2 << std::endl;
+  }
+
+  return 0;
+}
